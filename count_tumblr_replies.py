46fd5114802fedd9baefc5e344df43a1a5a851a7
Desman735 <desman735.dev@gmail.com>
2022-04-09 01:00:06 +0300

    Rework script to extract options from the initial post, remove some false positive results, add more info during counting process

--- count_tumblr_replies.py
+++ count_tumblr_replies.py
@@ -1,7 +1,31 @@
 import re
+from html.parser import HTMLParser
 
 from dynaconf import Dynaconf
 from pytumblr import TumblrRestClient
+
+
+class PostCaptionParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.options = list()
+        self.handled_list = False
+        self.inside_list_option = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'ol':
+            assert not self.handled_list
+            self.handled_list = True
+        if tag == 'li':
+            self.inside_list_option = True
+
+    def handle_endtag(self, tag):
+        if tag == 'li':
+            self.inside_list_option = False
+
+    def handle_data(self, data):
+        if self.inside_list_option:
+            self.options.append(data)
 
 
 def notes_iter(tumblr_client, blogname, notes_post_id, filter_type):
@@ -17,6 +41,18 @@ def notes_iter(tumblr_client, blogname, notes_post_id, filter_type):
         handled_notes += len(notes['notes'])
         if handled_notes < total_notes:
             notes = client.notes(blogname=blog, id=post_id, before_timestamp=last_handled_timestamp)
+
+
+def get_post_options(tumblr_client, blogname, post_id):
+    request = tumblr_client.posts(blogname=blogname, id=post_id)
+    assert len(request['posts']) == 1
+    post_html = request['posts'][0]['caption']
+    parser = PostCaptionParser()
+    parser.feed(post_html)
+    result = dict()
+    for index, option in enumerate(parser.options):
+        result[str(index + 1)] = option
+    return result
 
 
 def format_reply(line: str) -> str:
@@ -42,18 +78,27 @@ client = TumblrRestClient(key)
 
 replies = dict()
 
-print("Counting replies...")
+
+print("Getting choices...")
+options = get_post_options(client, blog, post_id)
+for option_number, option_text in options.items():
+    print(f"Choice {option_number}: {option_text}")
+
+print("\nCounting replies...")
 for reply in notes_iter(client, blog, post_id, 'reply'):
     reply_author = reply['blog_name']
     reply_text = reply['reply_text']
 
     if reply_author in replies:
+        if replies[reply_author] == reply_text:
+            continue
         replies[reply_author] += "\n"
         replies[reply_author] += reply_text
     else:
         replies[reply_author] = reply_text
+print(f"{len(replies)} replies to handle")
 
-print("Counting reblogs...")
+print("\nCounting reblogs...")
 for reblog_note in notes_iter(client, blog, post_id, 'reblog'):
     reblog_author = reblog_note['blog_name']
     reblog_post_id = reblog_note['post_id']
@@ -65,38 +110,44 @@ for reblog_note in notes_iter(client, blog, post_id, 'reblog'):
     assert len(reblogs) == 1
     reblog_post = reblogs[0]
     reblog_comment = reblog_post['reblog']['comment']
-    if reblog_comment:
-        if reblog_author in replies:
-            replies[reblog_author] += "\n"
-            replies[reblog_author] += reblog_comment
-        else:
-            replies[reblog_author] = reblog_comment
+    if not reblog_comment:
+        continue
+    reblog_comment: str
+    assert reblog_comment.startswith("<p>")
+    assert reblog_comment.endswith("</p>")
+    reblog_comment = reblog_comment[3:-4]
+    if reblog_author in replies:
+        if replies[reply_author] == reply_text:
+            continue
+        replies[reblog_author] += "\n"
+        replies[reblog_author] += reblog_comment
+    else:
+        replies[reblog_author] = reblog_comment
+print(f"{len(replies)} replies to handle")
 
-print("Calculating results...")
-choices = config.choices
+print("\nCalculating results...")
 regexes = dict()
 defined_replies = dict()
 undefined_replies = []
 
-for name, keywords in choices.items():
-    defined_replies[name] = list()
-    for keyword in keywords:
-        assert keyword not in regexes
-        regexes[keyword] = re.compile(rf"\b{keyword}\b", re.IGNORECASE)
+for number, option_text in options.items():
+    defined_replies[number] = list()
+    assert number not in regexes
+    regexes[number] = re.compile(rf"\W*{number}\W*", re.IGNORECASE)
+    assert option_text not in regexes
+    regexes[option_text] = re.compile(rf"\W*{option_text}\W*", re.IGNORECASE)
 
 for author, reply in replies.items():
     reply_options = list()
-    for name, keywords in choices.items():
-        for keyword in keywords:
-            if regexes[keyword].search(reply):
-                reply_options.append(name)
-                break
-
-    if len(reply_options) == 1:
-        defined_replies[reply_options[0]].append((reply, author))
+    for number, option_text in options.items():
+        if regexes[number].fullmatch(reply):
+            defined_replies[number].append((reply, author))
+            break
+        if regexes[option_text].fullmatch(reply):
+            defined_replies[number].append((reply, author))
+            break
     else:
         undefined_replies.append((reply, author))
-
 
 print("Results:")
 for choice, replies in defined_replies.items():
