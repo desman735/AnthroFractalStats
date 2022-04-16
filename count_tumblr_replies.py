e09f7bef3f226638807cd06677ea8f07dc968cdd
Desman735 <desman735.dev@gmail.com>
2022-04-16 13:59:44 +0300

    Move options and notes to the separate classes, update variable naming according to convention

--- count_tumblr_replies.py
+++ count_tumblr_replies.py
@@ -29,166 +29,204 @@ class PostCaptionParser(HTMLParser):
             self.options.append(data)
 
 
-def notes_iter(tumblr_client, blogname, notes_post_id, filter_type):
-    notes = tumblr_client.notes(blogname=blogname, id=notes_post_id)
-    total_notes = notes['total_notes']
-    handled_notes = 0
-    while handled_notes < total_notes:
-        for note in notes['notes']:
-            last_handled_timestamp = note['timestamp']
-            if note['type'] == filter_type:
-                yield note
+class PostOptions:
 
-        handled_notes += len(notes['notes'])
-        if handled_notes < total_notes:
-            notes = client.notes(blogname=blog, id=post_id, before_timestamp=last_handled_timestamp)
+    def __init__(self, post) -> None:
+        self.post_id = post['id']
+        self.extract_text_regex = re.compile(r"\W*")
+        try:
+            self.load_saved_options()
+        except FileNotFoundError:
+            post_html = post['caption']
+            parser = PostCaptionParser()
+            parser.feed(post_html)
+
+            self.options = dict()
+            self.options["0"] = list()
+
+            for index, option_text in enumerate(parser.options):
+                option_number = str(index + 1)
+                clean_option = self.extract_text_regex.sub("", option_text).casefold()
+                self.options[option_number] = [
+                    (option_number, option_number),
+                    (option_text, clean_option),
+                    (f"{option_number}. {option_text}", option_number + clean_option),
+                ]
+
+            self.save_current_options()
+
+    def load_saved_options(self):
+        with open(f"options/{self.post_id}.json", "r", encoding="utf8") as options_file:
+            self.options = json.load(options_file)
+
+    def save_current_options(self):
+        # Default "0" and at least two choices
+        assert len(self.options) > 2
+        with open(f"options/{self.post_id}.json", "w", encoding="utf8") as options_file:
+            json.dump(fp=options_file, obj=self.options, indent="\t", ensure_ascii=False)
+
+    def print_current_options(self):
+        for option_number, option_variants in self.options.items():
+            if option_number == "0":
+                continue
+            print(f"Option {option_number}:")
+            for variant in option_variants:
+                print(f"\t{format_reply(variant[0])}")
+
+    def get_options(self):
+        return self.options.keys()
+
+    def get_option_for_reply(self, reply):
+        clean_reply = self.extract_text_regex.sub("", reply).casefold()
+        for number, option_variants in self.options.items():
+            for variant in option_variants:
+                if clean_reply == variant[1]:
+                    return number
+        else:
+            reply_option = None
+            while not reply_option:
+                reply_option = input(f"What's the option for '{format_reply(reply)}'?")
+                if reply_option in self.options:
+                    self.options[reply_option].append((reply, clean_reply))
+                    self.save_current_options()
+                    return reply_option
+                else:
+                    print(f"Option {reply_option} is not in options")
+                    reply_option = None
 
 
-def load_options(post_id):
-    with open(f"options/{post_id}.json", "r", encoding="utf8") as options_file:
-        return json.load(options_file)
+class PostNotes:
 
+    def __init__(self, tumblr_client, post_link) -> None:
+        self.client = tumblr_client
 
-def save_options(post_id, options):
-    with open(f"options/{post_id}.json", "w", encoding="utf8") as options_file:
-        json.dump(fp=options_file, obj=options, indent="\t", ensure_ascii=False)
+        post = post_link.removeprefix("https://")
+        self.blog, post = post.split('/', 1)
 
+        post = post.removeprefix("post/")
+        self.post_id = post.split('/')[0]
 
-def print_options(options):
-    for option_number, option_variants in options.items():
-        print(f"Option {option_number}:")
-        for variant in option_variants:
-            print(f"\t{format_reply(variant[0])}")
+        self.initial_post = None
+        self.cache_initial_post()
+        assert self.initial_post
 
+        self.cached_notes = list()
+        self.cache_post_notes()
+        assert len(self.cached_notes) > 0
 
-def get_post_options(tumblr_client, blogname, post_id):
-    try:
-        return load_options(post_id)
-    except FileNotFoundError:
-        request = tumblr_client.posts(blogname=blogname, id=post_id)
+    def cache_post_notes(self):
+        notes = self.client.notes(
+            blogname=self.blog,
+            id=self.post_id,
+        )
+        total_notes = notes['total_notes']
+
+        handled_notes = 0
+        last_handled_timestamp = None
+        while handled_notes < total_notes:
+            assert len(notes) > 0
+            for note in notes['notes']:
+                last_handled_timestamp = note['timestamp']
+                self.cached_notes.append(note)
+            handled_notes += len(notes['notes'])
+
+            if handled_notes < total_notes:
+                notes = self.client.notes(
+                    blogname=self.blog,
+                    id=self.post_id,
+                    before_timestamp=last_handled_timestamp,
+                )
+
+    def cache_initial_post(self):
+        request = self.client.posts(blogname=self.blog, id=self.post_id)
         assert len(request['posts']) == 1
-        post_html = request['posts'][0]['caption']
-        parser = PostCaptionParser()
-        parser.feed(post_html)
-        result = dict()
-        extract_text_regex = re.compile(r"\W*")
-        for index, option in enumerate(parser.options):
-            option_number = str(index + 1)
-            clean_option = extract_text_regex.sub("", option).casefold()
-            result[option_number] = [
-                (option_number, option_number),
-                (option, clean_option),
-                (f"{option_number}. {option}", option_number + clean_option),
-            ]
-        return result
+        self.initial_post = request['posts'][0]
+
+    def filter_notes(self, filter_type):
+        assert len(self.cached_notes) > 0
+        for note in self.cached_notes:
+            if not filter_type or note['type'] == filter_type:
+                yield note
 
 
 def format_reply(line: str) -> str:
     line = line.replace("\n", " ⮒ ")
-    return f'"{line}"'
+    return f'- {line}'
 
 
-config = Dynaconf(
+CONFIG = Dynaconf(
     settings_files=[
         "config.toml"
     ]
 )
 
-key = config.tumblr_key
-post = config.tumblr_post_link
+CLIENT = TumblrRestClient(CONFIG.tumblr_key)
 
-post = post.removeprefix("https://")
-blog, post = post.split('/', 1)
-post = post.removeprefix("post/")
-post_id = post.split('/')[0]
-
-client = TumblrRestClient(key)
-
-replies = dict()
-
+print("Getting post notes...")
+NOTES = PostNotes(CLIENT, CONFIG.tumblr_post_link)
+print(f"Cached {len(NOTES.cached_notes)} notes")
 
 print("Getting initial choices...")
-options = get_post_options(client, blog, post_id)
-print_options(options)
-save_options(post_id, options)
+OPTIONS = PostOptions(NOTES.initial_post)
+OPTIONS.print_current_options()
 
+REPLIES = dict()
 print("\nCounting replies...")
-for reply in notes_iter(client, blog, post_id, 'reply'):
-    reply_author = reply['blog_name']
-    reply_text = reply['reply_text']
+for REPLY in NOTES.filter_notes(filter_type='reply'):
+    REPLY_AUTHOR = REPLY['blog_name']
+    REPLY_TEXT = REPLY['reply_text']
 
-    if reply_author in replies:
-        if replies[reply_author] == reply_text:
+    if REPLY_AUTHOR in REPLIES:
+        if REPLIES[REPLY_AUTHOR] == REPLY_TEXT:
             continue
-        replies[reply_author] += "\n"
-        replies[reply_author] += reply_text
+        REPLIES[REPLY_AUTHOR] += "\n"
+        REPLIES[REPLY_AUTHOR] += REPLY_TEXT
     else:
-        replies[reply_author] = reply_text
-print(f"{len(replies)} replies to handle")
+        REPLIES[REPLY_AUTHOR] = REPLY_TEXT
+print(f"{len(REPLIES)} replies to handle")
 
-if config.count_reblogs:
+if CONFIG.count_reblogs:
     print("\nCounting reblogs...")
-    for reblog_note in notes_iter(client, blog, post_id, 'reblog'):
-        reblog_author = reblog_note['blog_name']
-        reblog_post_id = reblog_note['post_id']
-        posts = client.posts(blogname=f"{reblog_author}.tumblr.com", id=reblog_post_id)
-        if 'posts' not in posts:
-            assert posts['meta']['status'] == 404
+    for REBLOG_NOTE in NOTES.filter_notes(filter_type='reblog'):
+        REBLOG_AUTHOR = REBLOG_NOTE['blog_name']
+        REBLOG_POST_ID = REBLOG_NOTE['post_id']
+        POSTS = CLIENT.posts(blogname=f"{REBLOG_AUTHOR}.tumblr.com", id=REBLOG_POST_ID)
+        if 'posts' not in POSTS:
+            assert POSTS['meta']['status'] == 404
             continue
-        reblogs = posts['posts']
-        assert len(reblogs) == 1
-        reblog_post = reblogs[0]
-        reblog_comment = reblog_post['reblog']['comment']
-        if not reblog_comment:
+        REBLOGS = POSTS['posts']
+        assert len(REBLOGS) == 1
+        REBLOG_POST = REBLOGS[0]
+        REBLOG_COMMENT = REBLOG_POST['reblog']['comment']
+        if not REBLOG_COMMENT:
             continue
-        reblog_comment: str
-        assert reblog_comment.startswith("<p>")
-        assert reblog_comment.endswith("</p>")
-        reblog_comment = reblog_comment[3:-4]
-        if reblog_author in replies:
-            if replies[reblog_author] == reblog_comment:
+        REBLOG_COMMENT: str
+        assert REBLOG_COMMENT.startswith("<p>")
+        assert REBLOG_COMMENT.endswith("</p>")
+        REBLOG_COMMENT = REBLOG_COMMENT[3:-4]
+        if REBLOG_AUTHOR in REPLIES:
+            if REPLIES[REBLOG_AUTHOR] == REBLOG_COMMENT:
                 continue
-            replies[reblog_author] += "\n"
-            replies[reblog_author] += reblog_comment
+            REPLIES[REBLOG_AUTHOR] += "\n"
+            REPLIES[REBLOG_AUTHOR] += REBLOG_COMMENT
         else:
-            replies[reblog_author] = reblog_comment
-    print(f"{len(replies)} replies to handle")
+            REPLIES[REBLOG_AUTHOR] = REBLOG_COMMENT
+    print(f"{len(REPLIES)} replies to handle")
 
 print("\nCalculating results...")
-extract_text_regex = re.compile(r"\W*")
-defined_replies = dict()
+DEFINED_REPLIES = dict()
 
-for option_number, option_variants in options.items():
-    defined_replies[option_number] = list()
+for OPTION in OPTIONS.get_options():
+    DEFINED_REPLIES[OPTION] = list()
 
-for author, reply in replies.items():
-    clean_reply = extract_text_regex.sub("", reply).casefold()
-    for number, option_variants in options.items():
-        defined_reply = False
-        for variant in option_variants:
-            if clean_reply == variant[1]:
-                defined_replies[number].append((reply, author))
-                defined_reply = True
-                break
-        if defined_reply:
-            break
-    else:
-        reply_option = None
-        while not reply_option:
-            reply_option = input(f"What's the option for '{format_reply(reply)}'?")
-            if reply_option in options:
-                defined_replies[reply_option].append((reply, author))
-                options[reply_option].append((reply, clean_reply))
-            else:
-                if reply_option != "0":
-                    print(f"Option {reply_option} is not in options")
-                    reply_option = None
+for AUTHOR, REPLY in REPLIES.items():
+    REPLY_OPTION = OPTIONS.get_option_for_reply(REPLY)
+    DEFINED_REPLIES[REPLY_OPTION].append((REPLY, AUTHOR))
 
 print("\nUpdated choices:")
-print_options(options)
-save_options(post_id, options)
+OPTIONS.print_current_options()
 
 print("\nResults:")
-for choice, replies in defined_replies.items():
-    if replies:
-        print(f'{choice}: {len(replies)}')
+for OPTION, REPLIES in DEFINED_REPLIES.items():
+    if REPLIES and OPTION != "0":
+        print(f'{OPTION}: {len(REPLIES)}')
